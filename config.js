8e60eeb4d3fbd76a247f9a7d4f8a041d464a75c2
Graham-Reeve <grahamreeve86@gmail.com>
2026-04-28 12:13:55 +0100

    Update API endpoints in quiz configuration

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.QUIZ_CONFIG = {
   title: "Liverpool Maths Society Challenge Quiz",
   subtitle: "Enter your name, answer 20 questions, and see how many you get correct.",
-  resultsEndpoint: "https://script.google.com/macros/s/AKfycbwNaMYA5V1HAlAktiQFvtYMKvuw6Tjbshjuy9O39APnCIpJdWJWjGl2Auj8dYgTYa35VA/exec",
-  leaderboardEndpoint: "https://script.google.com/macros/s/AKfycbwNaMYA5V1HAlAktiQFvtYMKvuw6Tjbshjuy9O39APnCIpJdWJWjGl2Auj8dYgTYa35VA/exec",
+  resultsEndpoint: "https://script.google.com/macros/s/AKfycbwbzyLQxe0uOUqswPDgL6Ahte0SaKYFRR-0S9FzncRAuqzCEluA13VykJTcpVnFuYhf/exec",
+  leaderboardEndpoint: "https://script.google.com/macros/s/AKfycbwbzyLQxe0uOUqswPDgL6Ahte0SaKYFRR-0S9FzncRAuqzCEluA13VykJTcpVnFuYhf/exec",
   adminKey: "Gauss1777",
   competitionYear: "2026",
   wholeNumbersOnly: true
