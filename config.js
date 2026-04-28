b3d045c9a517d457459d48f8cf16cd71c8d44daa
Graham-Reeve <grahamreeve86@gmail.com>
2026-04-28 14:14:06 +0100

    Update quiz title in configuration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.QUIZ_CONFIG = {
-  title: "Liverpool Maths Society Challenge Quiz",
+  title: "Liverpool Maths Society Senior Challenge Quiz",
   subtitle: "Enter your name, answer 20 questions, and see how many you get correct.",
   resultsEndpoint: "https://script.google.com/macros/s/AKfycbwbzyLQxe0uOUqswPDgL6Ahte0SaKYFRR-0S9FzncRAuqzCEluA13VykJTcpVnFuYhf/exec",
   leaderboardEndpoint: "https://script.google.com/macros/s/AKfycbwbzyLQxe0uOUqswPDgL6Ahte0SaKYFRR-0S9FzncRAuqzCEluA13VykJTcpVnFuYhf/exec",
